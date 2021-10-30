e79ca4a7fe7a2316480371c1cfbf8445e4ab2891
Shae Simeoni <shaebsimeoni@gmail.com>
2021-10-30 19:41:04 -0400

    Initial Commit

---
+++ Pictafy/PictafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictafyApp.swift
+//  Pictafy
+//
+//  Created by Shae Simeoni on 2021-10-30.
+//
+
+import SwiftUI
+
+@main
+struct PictafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
